d72e2f5aa7ce43e74aa68571b00e79602ebbe6ad
Fu22210 <kariagin.tm@phystech.edu>
2025-03-18 09:34:52 +0300

    fix flip.py

--- datasets/height_vertex_dataset/flip.py
+++ datasets/height_vertex_dataset/flip.py
@@ -15,14 +15,13 @@ def flip(VUW):
     val, idx = torch.sort(VUW, dim=2)
     mask = val[..., 1:] == val[..., :-1]
     has_duplicates = mask.view(N, -1).any(dim=1)
+    result = VUW.clone()
     if has_duplicates.any():
         flat_pos = torch.argmax((
             (1 + torch.rand((N, 3, r-1), dtype=torch.float)) * mask
         ).view(N, -1), dim=1)
         channel, pos = flat_pos // (r - 1), flat_pos % (r - 1)
-        print(has_duplicates)
         idx_sel = idx[has_duplicates, channel[has_duplicates]]
-        print(idx_sel)
         pos_selected = pos[has_duplicates]
         j1 = idx_sel[torch.arange(idx_sel.size(0)), pos_selected]
         j2 = idx_sel[torch.arange(idx_sel.size(0)), (pos_selected + 1) % r]
@@ -30,8 +29,8 @@ def flip(VUW):
         c1 = (channel[has_duplicates] + 1 + torch.randint(0, 1+1, (has_duplicates.sum(),))) % 3
         c2 = 3 - c1 - channel[has_duplicates]
 
-        result = VUW.clone()
-
         result[has_duplicates, c1, j1] ^= result[has_duplicates, c1, j2]
         result[has_duplicates, c2, j2] ^= result[has_duplicates, c2, j1]
-        return result
+    return result
+    
+    
